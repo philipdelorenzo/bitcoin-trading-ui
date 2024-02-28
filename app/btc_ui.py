6c977225f67b263199b0f63556bf8a84478b847c
Philip De Lorenzo <philip.delorenzo@gmail.com>
2024-02-27 23:08:44 -0800

    chore: add change to display

--- app/btc_ui.py
+++ app/btc_ui.py
@@ -19,31 +19,6 @@ tables1, tables2 = st.columns([1,1])
 order_history = requests.get(f"{config.api_url}/crypto/order_history", headers=config.headers).json()
 
 with tables1:
-    st.title("Order History - Last 25")
-    df = pd.DataFrame(columns=[
-        "Currency",
-        "Time Stamp",
-        "Effective Price",
-        "Purchage Type",
-        "Coin Quantity",
-        "Average Price",
-        "Transaction Type"
-    ])
-
-    for i in range(25):
-        df.loc[i] = [
-            order_history[i]["currency_code"],
-            order_history[i]["last_transaction_at"],
-            '${:,.2f}'.format(float(order_history[i]["price"])),
-            order_history[i]["type"],
-            order_history[i]["quantity"],
-            '${:,.2f}'.format(float(order_history[i]["average_price"])),
-            order_history[i]["side"],
-            ]
-
-    st.dataframe(df)
-
-with tables2:
     st.title("Percentage Change")
     c1, c2, c3 = st.columns([1,1,1])
     for coin in current_invested_symbols:
@@ -68,17 +43,41 @@ with tables2:
             st.metric(label=f"{coin}: Current Price", value=_cp, delta='${:,.2f}'.format(float(current_price) * float(quantity)))
             st.caption(f"If you sold all your {coin} coins at the current price, this is your profit.")
 
-        st.markdown(f"<b>Coin: {coin}</b><br>Last Transaction: {last_transaction_date}<br>Purchased Price: {_pp}<br>Current Price: {current_price}<br>Quantity: {quantity}", unsafe_allow_html=True)
+with tables2:
+    st.title("Order History - Last 25")
+    df = pd.DataFrame(columns=[
+        "Currency",
+        "Date",
+        "Time",
+        "Effective Price",
+        "Purchage Type",
+        "Coin Quantity",
+        "Average Price",
+        "Transaction Type"
+    ])
+
+    for i in range(25):
+        df.loc[i] = [
+            order_history[i]["currency_code"],
+            order_history[i]["last_transaction_at"].split("T")[0],
+            order_history[i]["last_transaction_at"].split("T")[1].split(".")[0],
+            '${:,.2f}'.format(float(order_history[i]["price"])),
+            order_history[i]["type"],
+            order_history[i]["quantity"],
+            '${:,.2f}'.format(float(order_history[i]["average_price"])),
+            order_history[i]["side"],
+            ]
+
+    st.dataframe(df)
 
 # Let's add a separator
 st.markdown("""<hr style="height:px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
-st.title("BTC UI")
+st.title("Profile Data and Health")
 col1, col2, col3, col4 = st.columns([1,1,1,1], gap="small")
 
 with col1:
     st.metric(label="Crypto Investments", value=ph_data["crypto"]["equity"]["amount"])
-    # Let's calculate the owned coins in USD
 
 with col2:
     st.metric(label="Buying Power", value=ph_data["crypto_buying_power"]["amount"])
@@ -110,3 +109,18 @@ with sec3:
     st.title("Coin Investment - USD")
     for coin in current_invested_symbols:
         st.metric(label=coin, value='${:,.2f}'.format(float(quote_data[coin]["mark_price"]) * float(current_invested_symbols[coin])))
+
+######## Let's calculate the owned coins in USD ########
+for coin in current_invested_symbols:
+    purchased_price = last_transaction["price"]
+    current_price = quote_data[coin]["mark_price"]
+    percentage_change = '{:,.2f}'.format((float(current_price) - float(purchased_price)) / float(purchased_price) * 100)
+
+    _params = {
+        "symbol": coin,
+        "purchased_price": purchased_price,
+        "current_price": current_price,
+        "percentage_change": percentage_change
+        }
+    results = requests.post(f"{config.api_url}/markets/calculate", headers=config.headers, params=_params).json()
+    print(results)
